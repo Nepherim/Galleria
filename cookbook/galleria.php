d9ee4d795dffcddf7601abcb091907e835aa82e9
David Gilbert <david@solidgone.com>
2011-04-29 22:57:41 -0400

    * chg: Updated documentation. * chg: Removed use of Carousel script. * chg: change $gallery['list'] from ".pmGalleryWrapper ul" to ".pmGalleryWrapper" * chg: Upgraded to Galleria 1.2.3. ** new: Add option to open allow integration with image 'viewers' like [[Cookbook/ThickBox]], LightBox, etc. ** new: Allow multiple galleries on a page. ** Removes the use of Skins. Provides Themes, only one sample provide "Classic". ** All prior options removed, and [[http://galleria.aino.se/docs/1.2/options/ | ]new options are now added]]. ** Loose vertical carousels. * new: Added defaults for height and width. * new: Provide mechanism to capture Galleria javascript options from PHP settings. * bug: IE7 displays the UL list of images briefly before correctly showing the image thumbs.

--- cookbook/galleria.php
+++ cookbook/galleria.php
@@ -1,7 +1,6 @@
 <?php if (!defined('PmWiki')) exit();
-/* Updated code from http://www.pmwiki.org/wiki/Cookbook/Galleria and updated it to Galleria 1.2.3 from http://galleria.aino.se/
-Mauro Bieg, 25th April 2011 
-
+/*
+Ref: http://www.pmwiki.org/wiki/Cookbook/Galleria
 Usage in pmwiki:
 
 (:div id="gallery":)
@@ -12,38 +11,61 @@ Usage in pmwiki:
 
 */
 
-$RecipeInfo['pmGallery']['Version'] = '0.4';
-$RecipeInfo['pmGallery']['Date'] = '2011-04-25';
+$RecipeInfo['Galleria']['Version'] = '0.4.1';
+$RecipeInfo['Galleria']['Date'] = '2011-04-29';
 
 /**
 * Code executed on include
 */
-Markup('galleria', 'inline', "/\\(:galleria\\s*(.*?):\\)/se", "Keep(galleria(PSS('$1')))");
 
-SDV($galleria['skin'],'tango');
+// You can pass more options to galleria, either from config.php, or from the galleria markup. see http://galleria.aino.se/docs/1.2/options/
+SDVA($galleria,array(
+	'list'	=> '',		#If you're using the pmGallery Cookbook, use ".pmGalleryWrapper"
+	'width'	=> '500',
+	'height'	=> '500'
+));
+SDV($galleria_fn, 'galleria');	#Only set if you need to override the basic galleria javascript call
+Markup('galleria', 'inline', "/\\(:galleria\\s*(.*?):\\)/se", "Keep($galleria_fn(PSS('$1')))");
+
 $HTMLHeaderFmt['jquery'] = '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>';
-$HTMLHeaderFmt['galleria'] = '<script src="' . $PubDirUrl . '/galleria/galleria-1.2.3.js"></script><script type="text/javascript">Galleria.loadTheme("' . $PubDirUrl . '/galleria/themes/classic/galleria.classic.min.js");</script>';
+$HTMLHeaderFmt['galleria'] = '<script src="'. $PubDirUrl. '/galleria/galleria-1.2.3.js"></script>'.
+	'<script type="text/javascript">Galleria.loadTheme("'. $PubDirUrl. '/galleria/themes/classic/galleria.classic.min.js");</script>';
 
 function galleria ($args) {
-	$o = Array(
-		'list'		 		=> '',		// use ".pmGalleryWrapper" with pmGallery
-		'width' 			=> '',		// 500
-		'height' 			=> '',		// 500
-		//you can add more options to pass in pmwiki here, most probably you want to pass them along to galleria (see below)
-	);
-	$o = array_merge($o, $GLOBALS['galleria']);
-	$o = array_merge($o,ParseArgs($args));
+	$args = ParseArgs($args);
+	unset($args['#']);
 
+	$o = array_merge($GLOBALS['galleria'],$args);
+	$o_galleria = galleria_json_encode($o);
 	return '
 <script type="text/javascript">
-//<![CDATA[
 $(document).ready(function(){
-	$("'. $o['list']. '").galleria({
-		width:' . $o['width'] .',
-		height:' . $o['height'] .'
-		//here you can pass more options to galleria. see http://galleria.aino.se/docs/1.2/options/
-	});
+	$("'. $o['list']. '").galleria('. $o_galleria. ');
 });
-//]]>
 </script>';
+}
+
+#json_encode only in PHP5.2+. Rather than overriding json_encode, and supporting two versions. ref http://www.mike-griffiths.co.uk/php-json_encode-alternative/
+function galleria_json_encode($a=false){
+	if (is_null($a)) return 'null';
+	if ($a === false) return 'false';
+	if ($a === true) return 'true';
+	if (is_scalar($a)){
+		if (is_float($a) || is_numeric($a))  return floatval(str_replace(",", ".", strval($a)));  #Always use "." for floats.
+		if (is_string($a)) {
+			static $jsonReplaces = array(array("\\", "/", "\n", "\t", "\r", "\b", "\f", '"'), array('\\\\', '\\/', '\\n', '\\t', '\\r', '\\b', '\\f', '\"'));
+			return '"' . str_replace($jsonReplaces[0], $jsonReplaces[1], $a) . '"';
+		}else  return $a;
+	}
+	$isList = true;
+	for ($i = 0, reset($a); $i < count($a); $i++, next($a))
+		if (key($a) !== $i){ $isList = false; break; }
+	$result = array();
+	if ($isList){
+		foreach ($a as $v)  $result[] = galleria_json_encode($v);
+		return '[ ' . join(', ', $result) . ' ]';
+	}else{
+		foreach ($a as $k => $v) $result[] = galleria_json_encode($k).': '.galleria_json_encode($v);
+		return '{ ' . join(', ', $result) . ' }';
+	}
 }
