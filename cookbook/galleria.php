b91a1faee205f4418c1c8923c86a1910c8e3d180
David Gilbert <david@solidgone.com>
2011-06-22 14:16:31 -0400

    bug: Passing string based parameters via markup fails. chg: Provide a mechanism to specify javascript based options from markup. Use $galleria_safe_mode=false.

--- cookbook/galleria.php
+++ cookbook/galleria.php
@@ -22,15 +22,19 @@ Don't use this in conjunction with the (:galleria :) markup, use one or the othe
 >><<
 */
 
-$RecipeInfo['Galleria']['Version'] = '0.5.0';
-$RecipeInfo['Galleria']['Date'] = '2011-06-21';
+$RecipeInfo['Galleria']['Version'] = '0.5.1';
+$RecipeInfo['Galleria']['Date'] = '2011-06-22';
 
 // You can pass more options to galleria, either from config.php, or from the galleria markup. see http://galleria.aino.se/docs/1.2/options/
 SDVA($galleria,array(
-	'list' => '.galleria',  #If you're using the pmGallery Cookbook, use ".pmGalleryWrapper"
+	'list' => '.galleria',  #If you're using the pmGallery Cookbook, use ".pmGalleryImageList"
 	'width' => '500',
-	'height' => '500'
-));
+	'height' => '500'));
+
+// if your pages are locked, or you trust editors, set this true to allow unsafe options to be specified from markup. Otherwise unsafe options can be specified only from config.php
+SDV($galleria_safe_mode, true);
+SDVA($galleria_unsafe_options, array('dataConfig', 'extend'));  #galleria.js expects these options to be functions, and thus NOT quoted when passed into galleria(). Potentially unsafe.
+
 SDV($galleria_fn, 'galleria');  #Advanced use only. Set if you need to override the basic galleria javascript call.
 Markup('galleria', 'inline', "/\\(:galleria\\s*(.*?):\\)/se", "Keep($galleria_fn(PSS('$1')))");
 
@@ -40,19 +44,49 @@ SDVA($HTMLHeaderFmt, array(
 	'galleria-theme' => '<script type="text/javascript">Galleria.loadTheme("'. $PubDirUrl. '/galleria/themes/classic/galleria.classic.min.js");</script>'
 ));
 
-
 function galleria ($args=false) {
+global $galleria, $galleria_unsafe_options, $galleria_safe_mode;
+
 	$args = ParseArgs($args);
 	unset($args['#']);
-	$o = array_merge($GLOBALS['galleria'],$args);
+	if ($galleria_safe_mode)  #remove unsafe options from markup args -- only allow from config.php
+		foreach ($galleria_unsafe_options as $v)
+			unset($args[$v]);
 
-	foreach ($o as $k => $v)  if ($k != 'list')  $o_str[] = $k .':' .$v;
-	$o_str = trim(join(',', $o_str), ',');
+	$o = array_merge($galleria, $args);
 
 	return '
 <script type="text/javascript">
 $(document).ready(function(){
-	$("'. $o['list']. '").galleria({ ' .$o_str .' });
+	$("'. $o['list']. '").galleria(' . bi_json_encode($o, false, $galleria_unsafe_options).');
 });
 </script>';
+}
+
+
+#json_encode only in PHP5.2+. Rather than overriding json_encode, and supporting two versions. ref http://www.mike-griffiths.co.uk/php-json_encode-alternative/
+#$strict will wrap the key in quotes.
+#keys in $np array if found in $a will not have VALUES wrapped in quotes. Allows javascript functions to be option values.
+function bi_json_encode($a=false, $strict=true, $np=false){
+	if (is_null($a)) return 'null';
+	if ($a === false) return 'false';
+	if ($a === true) return 'true';
+	if (is_scalar($a)){
+		if (is_float($a) || is_numeric($a))  return floatval(str_replace(",", ".", strval($a)));  #Always use "." for floats.
+		if (is_string($a)) {
+			static $jsonReplaces = array(array("\\", "/", "\n", "\t", "\r", "\b", "\f", '"'), array('\\\\', '\\/', '\\n', '\\t', '\\r', '\\b', '\\f', '\"'));
+			return '"' . str_replace($jsonReplaces[0], $jsonReplaces[1], $a) . '"';
+		}else  return $a;
+	}
+	$isList = true;
+	for ($i = 0, reset($a); $i < count($a); $i++, next($a))
+		if (key($a) !== $i){ $isList = false; break; }
+	$result = array();
+	if ($isList){
+		foreach ($a as $v)  $result[] = bi_json_encode($v, $strict, $np);
+		return '[ ' . join(', ', $result) . ' ]';
+	}else{
+		foreach ($a as $k => $v) $result[] = ($strict ?bi_json_encode($k, $strict, $np) :$k) .': ' .(in_array($k, $np) ?$v :bi_json_encode($v, $strict, $np));
+		return '{ ' . join(', ', $result) . ' }';
+	}
 }
