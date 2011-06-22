bbc0cba5030b3f6f51e64d497fe6d50240289cb6
David Gilbert <david@solidgone.com>
2011-06-21 22:45:27 -0400

    bug: Passing some parameters (like height, and width) caused errors due to use of json_encode. Galleria requires that some attributes be passed as non-text (ie, not quoted). This change allows the use of javascript as parameters, ie for [[http://galleria.aino.se/docs/1.2/options/dataConfig/ | dataConfig]]. chg: Default list to '.galleria' class. chg: Update to [[http://galleria.aino.se/ | Galleria 1.2.4]].

--- cookbook/galleria.php
+++ cookbook/galleria.php
@@ -9,65 +9,50 @@ Usage in pmwiki:
 (:divend:)
 (:galleria list="#gallery" width="500" height="500":)
 
+
+OR, automatically pick up DIV with class .galleria set in $galleria, and enabling galleria at the top of all pages.
+Don't use this in conjunction with the (:galleria :) markup, use one or the other on a page.
+  $galleria['list']='.galleria';
+  include(...);
+  galleria();
+
+>>galleria<<
+* Attach:DAW/tangocrash.jpg"Some caption"
+* Attach:DAW/walcheturm_freeman.jpg"Some other caption"
+>><<
 */
 
-$RecipeInfo['Galleria']['Version'] = '0.4.1';
-$RecipeInfo['Galleria']['Date'] = '2011-05-5';
-
-/**
-* Code executed on include
-*/
+$RecipeInfo['Galleria']['Version'] = '0.5.0';
+$RecipeInfo['Galleria']['Date'] = '2011-06-21';
 
 // You can pass more options to galleria, either from config.php, or from the galleria markup. see http://galleria.aino.se/docs/1.2/options/
 SDVA($galleria,array(
-	'list'	=> '',		#If you're using the pmGallery Cookbook, use ".pmGalleryWrapper"
-	'width'	=> '500',
-	'height'	=> '500'
+	'list' => '.galleria',  #If you're using the pmGallery Cookbook, use ".pmGalleryWrapper"
+	'width' => '500',
+	'height' => '500'
 ));
-SDV($galleria_fn, 'galleria');	#Only set if you need to override the basic galleria javascript call
+SDV($galleria_fn, 'galleria');  #Advanced use only. Set if you need to override the basic galleria javascript call.
 Markup('galleria', 'inline', "/\\(:galleria\\s*(.*?):\\)/se", "Keep($galleria_fn(PSS('$1')))");
 
 SDVA($HTMLHeaderFmt, array(
 	'jquery' => '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>',
-	'galleria-js' => '<script src="'. $PubDirUrl. '/galleria/galleria-1.2.3.js"></script>',
+	'galleria-js' => '<script src="'. $PubDirUrl. '/galleria/galleria-1.2.4.js"></script>',
 	'galleria-theme' => '<script type="text/javascript">Galleria.loadTheme("'. $PubDirUrl. '/galleria/themes/classic/galleria.classic.min.js");</script>'
 ));
 
-function galleria ($args) {
+
+function galleria ($args=false) {
 	$args = ParseArgs($args);
 	unset($args['#']);
-
 	$o = array_merge($GLOBALS['galleria'],$args);
-	$o_galleria = galleria_json_encode($o);
+
+	foreach ($o as $k => $v)  if ($k != 'list')  $o_str[] = $k .':' .$v;
+	$o_str = trim(join(',', $o_str), ',');
+
 	return '
 <script type="text/javascript">
 $(document).ready(function(){
-	$("'. $o['list']. '").galleria('. $o_galleria. ');
+	$("'. $o['list']. '").galleria({ ' .$o_str .' });
 });
 </script>';
-}
-
-#json_encode only in PHP5.2+. Rather than overriding json_encode, and supporting two versions. ref http://www.mike-griffiths.co.uk/php-json_encode-alternative/
-function galleria_json_encode($a=false){
-	if (is_null($a)) return 'null';
-	if ($a === false) return 'false';
-	if ($a === true) return 'true';
-	if (is_scalar($a)){
-		if (is_float($a) || is_numeric($a))  return floatval(str_replace(",", ".", strval($a)));  #Always use "." for floats.
-		if (is_string($a)) {
-			static $jsonReplaces = array(array("\\", "/", "\n", "\t", "\r", "\b", "\f", '"'), array('\\\\', '\\/', '\\n', '\\t', '\\r', '\\b', '\\f', '\"'));
-			return '"' . str_replace($jsonReplaces[0], $jsonReplaces[1], $a) . '"';
-		}else  return $a;
-	}
-	$isList = true;
-	for ($i = 0, reset($a); $i < count($a); $i++, next($a))
-		if (key($a) !== $i){ $isList = false; break; }
-	$result = array();
-	if ($isList){
-		foreach ($a as $v)  $result[] = galleria_json_encode($v);
-		return '[ ' . join(', ', $result) . ' ]';
-	}else{
-		foreach ($a as $k => $v) $result[] = galleria_json_encode($k).': '.galleria_json_encode($v);
-		return '{ ' . join(', ', $result) . ' }';
-	}
 }
